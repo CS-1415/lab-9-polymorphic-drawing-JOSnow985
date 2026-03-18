437b3f0ca3c7c851a7bb2af892641769dd192dfb
JOSnow985 <jaden.olvera@students.snow.edu>
2026-03-17 20:20:34 -0600

    Circle Dimension Test passed

--- Lab09/Circle.cs
+++ Lab09/Circle.cs
@@ -3,12 +3,18 @@
 public class Circle : AbstractGraphic2D
 {
     public override decimal LowerBoundX => -1;
-
     public override decimal UpperBoundX => -1;
-
     public override decimal LowerBoundY => -1;
-
     public override decimal UpperBoundY => -1;
+    public decimal CenterX {get; private set;} = 1;
+    public decimal CenterY {get; private set;} = 1;
+    public decimal Radius {get; private set;} = 1;
+    public Circle(decimal center_x, decimal center_y, decimal radius)
+    {
+        CenterX = center_x;
+        CenterY = center_y;
+        Radius = radius;
+    }
 
     public override bool ContainsPoint(decimal x, decimal y)
     {
--- Lab09/obj/Debug/net8.0/Lab09.AssemblyInfo.cs
+++ Lab09/obj/Debug/net8.0/Lab09.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab09")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40fd15e5fbe4af7bf9a5098ac9f4e01a3e587eeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f796d130b94764bdd5c9cfc4a9029b8aab28f37")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab09")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab09")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
